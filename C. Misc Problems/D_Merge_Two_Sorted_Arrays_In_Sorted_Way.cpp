c4523c3a8f6a80ddcc1e17f8d8b60f9c27d59bbe
thesahibnanda <essenbeats@gmail.com>
2023-10-30 16:42:43 +0530

    feat: Approach Added Merge Two Sorted Vectors

--- C. Misc Problems/D_Merge_Two_Sorted_Arrays_In_Sorted_Way.cpp
+++ C. Misc Problems/D_Merge_Two_Sorted_Arrays_In_Sorted_Way.cpp
@@ -2,10 +2,9 @@
 #include <vector>
 #include <limits>
 
-
 class Solution {
 public:
-    std::vector<int> mergeNewVector(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+    std::vector<int> mergeNewVector(std::vector<int> nums1, int m, std::vector<int> nums2, int n) {
         std::vector<int> answerVector;
         int pointer1 = 0, pointer2 = 0;
 
@@ -19,19 +18,39 @@ public:
             }
         }
 
-        // Copy remaining elements from nums1, if any.
         while (pointer1 < m) {
             answerVector.push_back(nums1[pointer1]);
             pointer1++;
         }
 
-        // Copy remaining elements from nums2, if any.
         while (pointer2 < n) {
             answerVector.push_back(nums2[pointer2]);
             pointer2++;
         }
 
         return answerVector;
+    }
+
+    void mergeWithoutVector(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+        int pointer1 = m - 1, pointer2 = n - 1, mergedIdx = m + n - 1;
+
+        while (pointer1 >= 0 && pointer2 >= 0) {
+            if (nums1[pointer1] >= nums2[pointer2]) {
+                nums1[mergedIdx] = nums1[pointer1];
+                pointer1--;
+            } else {
+                nums1[mergedIdx] = nums2[pointer2];
+                pointer2--;
+            }
+            mergedIdx--;
+        }
+
+        // If there are remaining elements in nums2, copy them to nums1.
+        while (pointer2 >= 0) {
+            nums1[mergedIdx] = nums2[pointer2];
+            pointer2--;
+            mergedIdx--;
+        }
     }
 };
 
@@ -53,14 +72,23 @@ int main() {
         Vector2.push_back(n);
     }
 
-    std::vector<int> Answer = Solution().mergeNewVector(Vector1, Vector1.size(), Vector2, Vector2.size());
+    std::cout << "Solution Using Merging Approach That Requires New Vector" << std::endl;
+    std::vector<int> Answer1 = Solution().mergeNewVector(Vector1, Vector1.size(), Vector2, Vector2.size());
 
-    for (int num : Answer) {
-        std::cout << num << ", ";
+    for (int i = 0; i < Answer1.size(); i++) {
+        std::cout << Answer1[i];
+        if (i < Answer1.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+
+    std::cout << "\n\nSolution Using Merging Approach That Doesn't Require New Vector" << std::endl;
+    Solution().mergeWithoutVector(Vector1, Vector1.size(), Vector2, Vector2.size());
+
+    for(int i=0; i<Vector1.size()+Vector2.size(); i++)
+    {
+        std::cout<<Vector1[i]<<", ";
     }
 
     return 0;
 }
-
-
-// Approach To Merge Sorted Vectors In C++ By Using An Additional Vectors
